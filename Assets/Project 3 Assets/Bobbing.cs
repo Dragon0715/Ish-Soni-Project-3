adfefbd16541a75daddc16b025d6f8bef940bc7f
Dragon0715 <isoni3@uic.edu>
2022-11-22 22:22:29 -0600

    added custom candle, particle effect, and lighting

---
+++ Assets/Project 3 Assets/Bobbing.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bobbing : MonoBehaviour
+{   
+    Vector3 posOffSet = new Vector3();
+    Vector3 tempPos = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        posOffSet = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempPos = posOffSet;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI) * 0.25f;
+
+        transform.position = tempPos;
+    }
+}
